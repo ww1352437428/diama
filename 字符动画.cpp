828d26877cc0b2c345ef52adf3344b0e66267ef2
ww1352437428 <61954062+ww1352437428@users.noreply.github.com>
2020-03-13 14:58:01 +0800

    字符动画
    无

---
+++ 字符动画.cpp
@@ -0,0 +1,24 @@
+
+
+#include <iostream>
+#include<windows.h>
+#include <cmath>
+#include <stdlib.h >//?
+using namespace std;
+#define PI 3.1415926 //?
+#define K ((2*PI)/360) //?
+#define N 25//?
+void main() //?
+{
+system("color BD");
+cout <<endl <<endl <<endl;
+int x,y,i;
+for(x=0;x<=405;x=x+8)
+{
+y=float(N*sin(x*K-N))+N;
+for(i=0;i<y;i++)
+cout <<" ";
+cout <<"* " <<endl;
+Sleep(1000);
+}
+}
